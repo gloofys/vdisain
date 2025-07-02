3561e2887e16f75b9c6e68cb9dc3ab344589cada
fredb <fred.brosman06@gmail.com>
2025-07-02 21:34:16 +0300

    wip

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import "./styles/main.scss";
+
+const App = () => {
+    return (
+        <div className="app-layout">
+            <Header />
+
+            <main className="app-content">
+
+
+                {/* <Hero /> */}
+                {/* <About /> */}
+                {/* <Projects /> */}
+                {/* <Contact /> */}
+
+                <footer className="footer">
+                    <p></p>
+                </footer>
+            </main>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "../styles/components/Header.scss";
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <div className="header-container">
+            <header className="header">
+                <div className="header__left"></div>
+
+                <div className="header__logo">
+                    <a href="/" className="nav-logo">
+                        Fred Brosman {/* or replace with <img> if you prefer */}
+                    </a>
+                </div>
+
+                <div className="header__burger">
+                    <button
+                        className="header__burger-button shimmer fadeIn"
+                        onClick={() => setMenuOpen(!menuOpen)}
+                        aria-label="Open menu"
+                    >
+                        <i className="jki jki-burger-menu-light"></i>
+                        <span className="header__burger-text">Menüü</span>
+                    </button>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
