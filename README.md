2c13c9b27985919ca345c74276ceff1ea04de7bb
fredb <fred.brosman06@gmail.com>
2025-08-14 21:44:01 +0300

    Started ScrollHint component.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Mistakes I found on the vdisain website:
 
 1.Menu close button x should be white for max-width(1024px) not 767px
 2.Menüü button and burger button hover is not consistent. hover is set for upper div probably.
-3.
+3.visible small divider line under hero necessary?
---
+++ src/components/ScrollHint.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/components/ScrollHint.scss";
+
+const ScrollHint = () => {
+    return (
+        <div className="scroll-hint">
+            <span>Keri alla</span>
+            <div className="scroll-line" />
+        </div>
+    );
+};
+
+export default ScrollHint;
